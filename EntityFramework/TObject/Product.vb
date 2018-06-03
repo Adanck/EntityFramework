1dc0446664815eba3ce00ba50e219b2276eed4ef
Yerick Ramirez <yerick.ramirez@ucrso.info>
2018-06-03 17:54:56 -0600

    jaja

--- EntityFramework/TObject/Product.vb
+++ EntityFramework/TObject/Product.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Product
 
+    Sub New()
+
+    End Sub
+
+    Sub New(code As String, description As String, price As Double, amount As Integer)
+        Me.Code = code
+        Me.Description = description
+        Me.Price = price
+        Me.Amount = amount
+
+    End Sub
+    Public Property Code As String
+    Public Property Description As String
+    Public Property Price As Double
+    Public Property Amount As Integer
+
+
 End Class
---
+++ EntityFramework/TObject/TOClient.vb
@@ -0,0 +1,21 @@
+﻿Public Class TOClient
+
+    Sub New()
+
+    End Sub
+
+    Sub New(id As Integer, name As String, lastName As String, email As String, phoneNumber As Integer)
+        Me.Id = id
+        Me.Name = name
+        Me.LastName = lastName
+        Me.Email = email
+        Me.PhoneNumber = phoneNumber
+    End Sub
+
+    Public Property Id As Integer
+    Public Property Name As String
+    Public Property LastName As String
+    Public Property Email As String
+    Public Property PhoneNumber As Integer
+
+End Class
