7daec69664dc935a37e187a27d551c46ec70e4e5
Adan Talavera <adan_talavera@hotmail.com>
2018-06-03 17:49:02 -0600

    deer

---
+++ EntityFramework/BL/BLproduct.vb
@@ -0,0 +1,3 @@
+﻿Public Class BLproduct
+
+End Class
---
+++ EntityFramework/TObject/Factura.vb
@@ -0,0 +1,9 @@
+﻿Public Class Factura
+    'Facturación: Debe permitir al usuario crear facturas. Para esto, se selecciona un cliente existente
+    'en la base de datos y se procede a agregar una serie de líneas de detalle (Producto, cantidad, 
+    'precio unitario, sub total). Al finalizar la factura, toda esta información debe guardarse en la 
+    'base de datos junto con la fecha y hora de la creación de la factura (La fecha y hora se toman 
+    'del servidor de bases de datos). Los saldos de los productos deben quedar actualizados en la 
+    'base de datos.
+
+End Class
